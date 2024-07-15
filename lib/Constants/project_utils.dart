91d6eef2a57bb7c292a3f28c86445465616ba0bb
Abhijeet Singh <109523390+AbhijeetS29@users.noreply.github.com>
2024-07-15 21:09:12 +0530

    update

--- lib/Constants/project_utils.dart
+++ lib/Constants/project_utils.dart
@@ -23,7 +23,7 @@ List<ProjectUtils> workProjectUtils = [
     language: "assets/images/android.png",
     subtitle:
     'A synchronized mobile application and computer software based on a task management system, built to improve time management in people\'s daily lives.',
-    webLink: 'https://github.com/AbhijeetS29/TaskNinja',
+    webLink: 'https://github.com/AbhijeetS29/Task_Ninja_/tree/main',
     view: 'View on Github',
   ),
   ProjectUtils(
@@ -32,8 +32,8 @@ List<ProjectUtils> workProjectUtils = [
     language: "assets/images/flutter.png",
     subtitle:
     'Digital book app that allows offline access to books authored by Mr. Vivek Joshi, President of Madhav Sewa Society, by downloading and saving them locally.',
-    webLink: 'https://www.externally.unavailable.project',
-    view: 'View Project',
+    webLink: 'https://github.com/AbhijeetS29/Sarishti_Sanwaad_ReadMe',
+    view: 'View on Github',
   ),
   ProjectUtils(
     image: 'assets/images/presentation.png',
@@ -41,16 +41,16 @@ List<ProjectUtils> workProjectUtils = [
     language: "assets/images/flutter.png",
     subtitle:
     'Attendance and student management app that tracks entry/exit times, documents, daily tests, and announcements. Includes sections for volunteer details, tasks, and completion status.',
-    webLink: 'https://www.externally.unavailable.project',
-    view: 'View Project',
+    webLink: 'https://github.com/AbhijeetS29/MSS_StudentWing_Readme',
+    view: 'View on Github',
   ),
   ProjectUtils(
     image: 'assets/images/mudra.png',
     title: 'Soul Serenity',
     language: "assets/images/android.png",
     subtitle:
-    'Digital book app that allows offline access to books authored by Mr. Vivek Joshi, President of Madhav Sewa Society, by downloading and saving them locally.',
-    webLink: 'https://www.externally.unavailable.project',
+    'Application that holds Mudras, daily motivational videos prepared by Vivek Joshi.Each Mudra is explained in English, Hindi, Punjabi',
+    webLink: 'https://github.com/AbhijeetS29/soul_serenity_readme',
     view: 'View on Github',
   ),
   ProjectUtils(
@@ -59,7 +59,7 @@ List<ProjectUtils> workProjectUtils = [
     language: "assets/images/flutter.png",
     subtitle:
     'A random project where scrap collectors can calculate the price of waste collected by entering its weight and rate per weight of various objects.',
-    webLink: 'https://github.com/AbhijeetS29/Scrap_calculator',
+    webLink: 'https://github.com/AbhijeetS29/scrap_calculator_readme',
     view: 'View on Github',
   ),
 ];
